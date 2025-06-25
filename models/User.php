92b74220f7d9f8e4ee4bd6d97690288afb52119f
Osint22 <davidhavrilec@gmail.com>
2025-06-25 13:10:03 +0000

    Fixed bugs etape 4 - ID = NULL

--- models/User.php
+++ models/User.php
@@ -1,42 +1,53 @@
 <?php
 class User {
-   private int $id;
-   private string $email;
-   private string $firstName;
-   private string $lastName;
-   
-   public function __construct(
-       int $id ,
-       string $email ,
-       string $firstName ,
-       string $lastName
-       )
- {
-    $this->id = $id;
-    $this->email = $email;
-    $this->firstName = $firstName;
-    $this->lastName = $lastName;
- 
-}
+    private ?int $id; 
+    private string $email;
+    private string $firstName;
+    private string $lastName;
 
- 
-    public function getId(): int
-    {
+    public function __construct(
+        ?int $id = null, 
+        string $email,
+        string $firstName,
+        string $lastName
+    ) {
+        $this->id = $id;
+        $this->email = $email;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getEmail(): string
-    {
+    public function getEmail(): string {
         return $this->email;
     }
 
-    public function getFirstName(): string
-    {
+    public function getFirstName(): string {
         return $this->firstName;
     }
 
-    public function getLastName(): string
-    {
+    public function getLastName(): string {
         return $this->lastName;
+    }
+
+   
+    public function setId(?int $id): void {
+        $this->id = $id;
+    }
+
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    public function setFirstName(string $firstName): void {
+        $this->firstName = $firstName;
+    }
+
+    public function setLastName(string $lastName): void {
+        $this->lastName = $lastName;
     }
 }
